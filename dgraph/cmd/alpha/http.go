ee023d3da8af1fe80a595edafcca68e971bbdde0
Javier Alvarado <javier@dgraph.io>
2018-12-21 17:13:30 -0800

    Rewrite common handler, add compression support.

--- dgraph/cmd/alpha/http.go
+++ dgraph/cmd/alpha/http.go
@@ -18,9 +18,11 @@ package alpha
 
 import (
 	"bytes"
+	"compress/gzip"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"sort"
@@ -29,13 +31,14 @@ import (
 	"time"
 
 	"github.com/dgraph-io/dgo/protos/api"
+	"github.com/golang/glog"
+	"google.golang.org/grpc/metadata"
+
 	"github.com/dgraph-io/dgraph/edgraph"
 	"github.com/dgraph-io/dgraph/gql"
 	"github.com/dgraph-io/dgraph/query"
 	"github.com/dgraph-io/dgraph/worker"
 	"github.com/dgraph-io/dgraph/x"
-	"github.com/golang/glog"
-	"google.golang.org/grpc/metadata"
 )
 
 func allowed(method string) bool {
@@ -60,9 +63,9 @@ func extractStartTs(urlPath string) (uint64, error) {
 	}
 }
 
-// Common functionality for these request handlers. Returns true if the request is completely
+/*// Common functionality for these request handlers. Returns true if the request is completely
 // handled and nothing further needs to be done.
-func commonHandler(w http.ResponseWriter, r *http.Request) bool {
+func commonHandlerOld(w http.ResponseWriter, r *http.Request) bool {
 	// Do these requests really need CORS headers? Doesn't seem like it, but they are probably
 	// harmless aside from the extra size they add to each response.
 	x.AddCorsHeaders(w)
@@ -80,12 +83,107 @@ func commonHandler(w http.ResponseWriter, r *http.Request) bool {
 	return false
 }
 
+// Common functionality for these request handlers. Returns a nil writer if the request is
+// completely handled and nothing further needs to be done.
+func commonHandlerOld2(w http.ResponseWriter, r *http.Request) (io.Writer, io.Reader) {
+	// Do these requests really need CORS headers? Doesn't seem like it, but they are probably
+	// harmless aside from the extra size they add to each response.
+	x.AddCorsHeaders(w)
+
+	if r.Method == "OPTIONS" {
+		return nil, nil
+	} else if !allowed(r.Method) {
+		w.WriteHeader(http.StatusBadRequest)
+		x.SetStatus(w, x.ErrorInvalidMethod, "Invalid method")
+		return nil, nil
+	}
+
+	var err error
+	rd := r.Body
+	if enc := r.Header.Get("Content-Encoding"); enc != "" && enc != "identity" {
+		if enc == "gzip" {
+			rd, err = gzip.NewReader(rd)
+			if err != nil {
+				x.SetStatus(w, x.Error, "Unable to create decompressor")
+				return nil, nil
+			}
+		} else {
+			x.SetStatus(w, x.ErrorInvalidRequest, "Unsupported content encoding")
+			return nil, nil
+		}
+	}
+
+	wr := w
+
+	w.Header().Set("Content-Type", "application/json")
+
+	return wr, rd
+}*/
+
+type streams struct {
+	in   io.Reader
+	out  io.Writer
+	clos []io.Closer
+}
+
+func (s *streams) close() {
+	for _, s := range s.clos {
+		_ = s.Close()
+	}
+}
+
+// Common functionality for these request handlers. Returns nil writer if the request is
+// completely handled here and nothing further needs to be done.
+func commonHandler(w http.ResponseWriter, r *http.Request) *streams {
+	// Do these requests really need CORS headers? Doesn't seem like it, but they are probably
+	// harmless aside from the extra size they add to each response.
+	x.AddCorsHeaders(w)
+
+	if r.Method == "OPTIONS" {
+		return nil
+	} else if !allowed(r.Method) {
+		w.WriteHeader(http.StatusBadRequest)
+		x.SetStatus(w, x.ErrorInvalidMethod, "Invalid method")
+		return nil
+	}
+
+	// These are the readers and writers the caller should use for the request and response
+	// body in order to handle compressed and uncompressed bodies transparently.
+	s := &streams{in: r.Body, out: w}
+
+	if enc := r.Header.Get("Content-Encoding"); enc != "" && enc != "identity" {
+		if enc == "gzip" {
+			gz, err := gzip.NewReader(s.in)
+			if err != nil {
+				x.SetStatus(w, x.Error, "Unable to create decompressor")
+				return nil
+			}
+			s.clos = append(s.clos, gz)
+		} else {
+			x.SetStatus(w, x.ErrorInvalidRequest, "Unsupported content encoding")
+			return nil
+		}
+	}
+
+	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		s.clos = append(s.clos, gz)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	return s
+}
+
 // This method should just build the request and proxy it to the Query method of dgraph.Server.
 // It can then encode the response as appropriate before sending it back to the user.
 func queryHandler(w http.ResponseWriter, r *http.Request) {
-	if commonHandler(w, r) {
+	s := commonHandler(w, r)
+	if s == nil {
 		return
 	}
+	defer s.close()
 
 	req := api.Request{}
 	ts, err := extractStartTs(r.URL.Path)
@@ -104,8 +202,7 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	defer r.Body.Close()
-	q, err := ioutil.ReadAll(r.Body)
+	q, err := ioutil.ReadAll(s.in)
 	if err != nil {
 		x.SetStatus(w, x.ErrorInvalidRequest, err.Error())
 		return
@@ -170,16 +267,17 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 		writeEntry("data", resp.Json)
 	}
 	out.WriteRune('}')
-	w.Write(out.Bytes())
+	s.out.Write(out.Bytes())
 }
 
 func mutationHandler(w http.ResponseWriter, r *http.Request) {
-	if commonHandler(w, r) {
+	s := commonHandler(w, r)
+	if s == nil {
 		return
 	}
+	defer s.close()
 
-	defer r.Body.Close()
-	m, err := ioutil.ReadAll(r.Body)
+	m, err := ioutil.ReadAll(s.in)
 	if err != nil {
 		x.SetStatus(w, x.ErrorInvalidRequest, err.Error())
 		return
@@ -264,13 +362,15 @@ func mutationHandler(w http.ResponseWriter, r *http.Request) {
 		x.SetStatusWithData(w, x.Error, err.Error())
 		return
 	}
-	w.Write(js)
+	s.out.Write(js)
 }
 
 func commitHandler(w http.ResponseWriter, r *http.Request) {
-	if commonHandler(w, r) {
+	s := commonHandler(w, r)
+	if s == nil {
 		return
 	}
+	defer s.close()
 
 	resp := &api.Assigned{}
 	tc := &api.TxnContext{}
@@ -290,8 +390,7 @@ func commitHandler(w http.ResponseWriter, r *http.Request) {
 	tc.StartTs = ts
 
 	// Keys are sent as an array in the body.
-	defer r.Body.Close()
-	keys, err := ioutil.ReadAll(r.Body)
+	keys, err := ioutil.ReadAll(s.in)
 	if err != nil {
 		x.SetStatus(w, x.ErrorInvalidRequest, err.Error())
 		return
@@ -329,13 +428,15 @@ func commitHandler(w http.ResponseWriter, r *http.Request) {
 		x.SetStatusWithData(w, x.Error, err.Error())
 		return
 	}
-	w.Write(js)
+	s.out.Write(js)
 }
 
 func abortHandler(w http.ResponseWriter, r *http.Request) {
-	if commonHandler(w, r) {
+	s := commonHandler(w, r)
+	if s == nil {
 		return
 	}
+	defer s.close()
 
 	resp := &api.Assigned{}
 	tc := &api.TxnContext{}
@@ -370,18 +471,19 @@ func abortHandler(w http.ResponseWriter, r *http.Request) {
 		x.SetStatusWithData(w, x.Error, err.Error())
 		return
 	}
-	w.Write(js)
+	s.out.Write(js)
 }
 
 func alterHandler(w http.ResponseWriter, r *http.Request) {
-	if commonHandler(w, r) {
+	s := commonHandler(w, r)
+	if s == nil {
 		return
 	}
+	defer s.close()
 
 	op := &api.Operation{}
 
-	defer r.Body.Close()
-	b, err := ioutil.ReadAll(r.Body)
+	b, err := ioutil.ReadAll(s.in)
 	if err != nil {
 		x.SetStatus(w, x.ErrorInvalidRequest, err.Error())
 		return
@@ -418,7 +520,7 @@ func alterHandler(w http.ResponseWriter, r *http.Request) {
 		x.SetStatus(w, x.Error, err.Error())
 		return
 	}
-	w.Write(js)
+	s.out.Write(js)
 }
 
 // skipJSONUnmarshal stores the raw bytes as is while JSON unmarshaling.
